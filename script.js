7b282e3f3422cb5e64a32f03aea943176d7612ec
Pratham Saha <prathamsaha15@gmail.com>
2026-05-18 01:08:21 +0530

    Update block dimensions to 30px for improved gameplay consistency

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const board = document.querySelector('.board');
-const blockHeight = 50;
-const blockWidth = 50;
+const blockHeight = 30;
+const blockWidth = 30;
 
 const cols = Math.floor(board.clientWidth/blockWidth);
 const rows= Math.floor(board.clientHeight/blockHeight);

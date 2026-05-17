83ae820ab5c6fca19bf41125aaeb66d2d02233ff
Pratham Saha <prathamsaha15@gmail.com>
2026-05-18 01:02:12 +0530

    Update game board dimensions and styles for Snake Game

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const board = document.querySelector('.board');
-const blockHeight = 30;
-const blockWidth = 30;
+const blockHeight = 80;
+const blockWidth = 80;
 
 const cols = Math.floor(board.clientWidth/blockWidth);
-const rows = Math.floor(board.clientHeight/blockHeight);
+const rows= Math.floor(board.clientHeight/blockHeight);
+
+const blocks = [];
+const snake= [{x:1,y:3},{x:1,y:4},{x:1,y:5}];
 
 
-for(let i=0; i<rows*cols; i++){
-    const block = document.createElement('div');
-    block.classList.add("block");
-    board.appendChild(block);
-}
+for(let i=0; i<rows; i++){
+    for(let j=0; j<cols; j++){
+        /*create a div like<div></div>*/
+        const block = document.createElement('div');
+        /*<div class="block"></div>*/
+        block.classList.add("block");
+
+
+        // <div class="board">
+        // <div class="block"></div>    ...
+        // </div>
+        board.appendChild(block);
+        block.innerText = `${i}-${j}`;  
+        blocks[`${i}-${j}`] = block; 
+    }
+} 
+
+
+function render(){
+        snake.forEach(segment => {
+             blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+        });
+
+    }
+
+    render();
